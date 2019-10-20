5b1404565620b7baa9bc9ffde9a0af4785e3567e
Jae Eun Na <lette1394@gmail.com>
2019-10-20 21:53:32 +0900

    fix health check scripts

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 30s
+
 RESPONSE=$(curl --write-out %{http_code} --silent --output /dev/null "localhost:5000/l7_health_check")
 if [ $RESPONSE -eq 200 ]; then
 	echo 'server is running'
