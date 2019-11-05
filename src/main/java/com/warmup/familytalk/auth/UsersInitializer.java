bbe1825f85455247af76de04f9f078397e82fb4f
Jaeeun Na <lette1394@gmail.com>
2019-11-06 02:08:45 +0900

    remove csv

--- src/main/java/com/warmup/familytalk/auth/UsersInitializer.java
+++ src/main/java/com/warmup/familytalk/auth/UsersInitializer.java
@@ -1,9 +1,13 @@
 package com.warmup.familytalk.auth;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.warmup.familytalk.auth.model.Auth;
 import com.warmup.familytalk.auth.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -17,13 +21,26 @@ public class UsersInitializer implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        Auth.RegisterRequest user = new Auth.RegisterRequest("user",
-                                                             "password",
-                                                             "password",
-                                                             "kr",
-                                                             "1",
-                                                             "EN");
-        userService.createUser(user)
-                   .subscribe(it -> log.debug("Create user: {}", it));
+        Flux<Auth.RegisterRequest> user = Flux.just(
+                new Auth.RegisterRequest("user",
+                                         "password",
+                                         "password",
+                                         "kr",
+                                         "1",
+                                         "EN"),
+                new Auth.RegisterRequest("testuser1",
+                                         "password",
+                                         "password",
+                                         "kr",
+                                         "2",
+                                         "EN"),
+                new Auth.RegisterRequest("testuser2",
+                                         "password",
+                                         "password",
+                                         "kr",
+                                         "3",
+                                         "EN"));
+        user.map(userService::createUser)
+            .subscribe(it -> log.debug("Create user: {}", it));
     }
 }
--- src/main/java/com/warmup/familytalk/auth/repository/UserRepository.java
+++ src/main/java/com/warmup/familytalk/auth/repository/UserRepository.java
@@ -1,20 +1,11 @@
 package com.warmup.familytalk.auth.repository;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
-import javax.annotation.PostConstruct;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Repository;
-import com.warmup.familytalk.auth.model.Role;
 import com.warmup.familytalk.auth.model.User;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
 import reactor.core.publisher.Mono;
 
 @Repository
@@ -32,52 +23,9 @@ public class UserRepository {
     }
 
     public Mono<User> save(final User user) {
-        try {
-            FileWriter pw = new FileWriter(new ClassPathResource("/user.csv").getFile(), true);
-            user.setUserId(userIdCount.getAndIncrement());
-            pw.append("\n");
-            pw.append(user.toCsvFormat());
-            pw.flush();
-            userMapByName.put(user.getUsername(), user);
-            userMapById.put(user.getUserId(), user);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        user.setUserId(userIdCount.getAndIncrement());
+        userMapByName.put(user.getUsername(), user);
+        userMapById.put(user.getUserId(), user);
         return Mono.just(user);
-    }
-
-    @PostConstruct
-    private void initialize() {
-        try {
-            Reader in = new FileReader(new ClassPathResource("/user.csv").getFile());
-            Iterable<CSVRecord> records = CSVFormat.DEFAULT
-                    .withHeader("userId", "username", "password", "country", "profileImageNumber", "enabled", "roles")
-                    .withTrim()
-                    .withFirstRecordAsHeader()
-                    .parse(in);
-            for (CSVRecord record : records) {
-                long userId = Long.parseLong(record.get("userId"));
-                String username = record.get("username");
-                String password = record.get("password");
-                String country = record.get("country");
-                String profileImageNumber = record.get("profileImageNumber");
-                String enabled = record.get("enabled");
-                String roles = record.get("roles");
-
-                User user = new User(userId,
-                                     username,
-                                     password,
-                                     country,
-                                     profileImageNumber,
-                                     Boolean.parseBoolean(enabled),
-                                     Role.valueOf(roles));
-                userMapById.put(userId, user);
-                userMapByName.put(username, user);
-
-                userIdCount.incrementAndGet();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
